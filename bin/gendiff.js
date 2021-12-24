259c3bb468bd8e84fc049d505681e122e94309a3
Dmitry Kokorev <superflick3@gmail.com>
2021-12-24 17:46:50 +0300

    Add actions to gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,14 +1,18 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs'
+import findDiff from '../src/findDiff.js';
 
 const program = new Command();
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .option('-v, --version ', '0.0.1')
+  .version('0.0.1', '-V, --version', 'output the version number')
   .option('-f, --format [type]', 'output format')
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(findDiff(filepath1, filepath2));
+  });
     
 
 program.parse();
