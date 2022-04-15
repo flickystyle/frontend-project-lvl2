50de388bac9dcc7655fcf13df3ef3975ffcf4ef5
Dmitry Kokorev <superflick3@gmail.com>
2022-04-15 16:34:07 +0300

    Refactoring findDiff.js

--- src/findDiff.js
+++ src/findDiff.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
 
-const makeItemsEmpty = (item) => {
+const makeNodesEmpty = (item) => {
   if (typeof item !== 'object' || item === null) {
     return item;
   }
 
   const entries = Object.entries(item);
-  const result = entries.reduce((acc, [key, value]) => ({ ...acc, [key]: ['unchanged', makeItemsEmpty(value)] }), {});
+  const result = entries.reduce((acc, [key, value]) => ([...acc, { name: key, difference: 'unchanged', value: makeNodesEmpty(value) }]), []);
   return result;
 };
 
@@ -18,24 +18,26 @@ const findDiff = (obj1, obj2) => {
   const sortedKeys = _.sortBy(keys);
 
   const result = sortedKeys.reduce((acc, key) => {
-    const itemsWithEmptyDiff1 = makeItemsEmpty(obj1[key]);
-    const itemsWithEmptyDiff2 = makeItemsEmpty(obj2[key]);
+    const nodeWithEmptyDiff1 = makeNodesEmpty(obj1[key]);
+    const nodeWithEmptyDiff2 = makeNodesEmpty(obj2[key]);
 
     if (_.has(obj1, key) && !_.has(obj2, key)) {
-      return { ...acc, [key]: ['removed', itemsWithEmptyDiff1] };
+      return [...acc, { name: key, difference: 'removed', value: nodeWithEmptyDiff1 }];
     }
     if (!_.has(obj1, key) && _.has(obj2, key)) {
-      return { ...acc, [key]: ['added', itemsWithEmptyDiff2] };
+      return [...acc, { name: key, difference: 'added', value: nodeWithEmptyDiff2 }];
     }
     if ((typeof obj1[key] === 'object') && (typeof obj2[key] === 'object')) {
-      return { ...acc, [key]: ['unchanged', findDiff(obj1[key], obj2[key])] };
+      return [...acc, { name: key, difference: 'unchanged', value: findDiff(obj1[key], obj2[key]) }];
     }
     if (obj1[key] !== obj2[key]) {
-      return { ...acc, [key]: ['updated', itemsWithEmptyDiff1, itemsWithEmptyDiff2] };
+      return [...acc, {
+        name: key, difference: 'updated', value: nodeWithEmptyDiff1, updatedValue: nodeWithEmptyDiff2,
+      }];
     }
 
-    return { ...acc, [key]: ['unchanged', itemsWithEmptyDiff1] };
-  }, {});
+    return [...acc, { name: key, difference: 'unchanged', value: nodeWithEmptyDiff1 }];
+  }, []);
 
   return result;
 };
