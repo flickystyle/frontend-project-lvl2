ae98ef80c2df457221efd45abf0fa1bf231f7d99
Dmitry Kokorev <superflick3@gmail.com>
2022-05-17 14:11:21 +0300

    Refactoring stylish.js formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,41 +1,38 @@
 import { generateReplacer, formatValue } from './replacerAndFormat.js';
 
-const stylish = (node) => {
+const stylish = (node, depth = 1) => {
   const spacesCount = 4;
-  const iter = (currentNode, depth) => {
-    const diffIndentSize = 2;
-    const indentSize = (spacesCount * depth) - diffIndentSize;
-    const currentIndent = generateReplacer(indentSize);
 
-    const previousDepth = depth - 1;
-    const bracketIndentSize = spacesCount * previousDepth;
-    const bracketIndent = generateReplacer(bracketIndentSize);
+  const diffIndentSize = 2;
+  const indentSize = (spacesCount * depth) - diffIndentSize;
+  const currentIndent = generateReplacer(indentSize);
 
-    const lines = currentNode.children
-      .map((child) => {
-        const currentStatus = child.status;
-        switch (currentStatus) {
-          case 'nested':
-            return `${currentIndent}  ${child.name}: ${iter(child.children, depth + 1)}`;
-          case 'unchanged':
-            return `${currentIndent}  ${child.name}: ${formatValue(child.value, depth + 1)}`;
-          case 'removed':
-            return `${currentIndent}- ${child.name}: ${formatValue(child.value, depth + 1)}`;
-          case 'added':
-            return `${currentIndent}+ ${child.name}: ${formatValue(child.value, depth + 1)}`;
-          case 'updated':
-            return `${currentIndent}- ${child.name}: ${formatValue(child.oldValue, depth + 1)}\n${currentIndent}+ ${child.name}: ${formatValue(child.newValue, depth + 1)}`;
-          default:
-            throw new Error(`Unknown difference: '${currentStatus}'!`);
-        }
-      });
-    return [
-      '{',
-      ...lines,
-      `${bracketIndent}}`,
-    ].join('\n');
-  };
-  return iter(node, 1);
+  const previousDepth = depth - 1;
+  const bracketIndentSize = spacesCount * previousDepth;
+  const bracketIndent = generateReplacer(bracketIndentSize);
+
+  const currentStatus = node.status;
+  const currentChild = node.children;
+
+  switch (currentStatus) {
+    case 'root':
+      return ['{',
+        ...currentChild.map((child) => stylish(child)), `${bracketIndent}}`,
+      ].join('\n');
+    case 'nested':
+      return [`${currentIndent}  ${node.name}: {`, ...currentChild.children.map((child) => stylish(child, depth + 1)), `${bracketIndent}    }`,
+      ].join('\n');
+    case 'unchanged':
+      return `${currentIndent}  ${node.name}: ${formatValue(node.value, depth + 1)}`;
+    case 'removed':
+      return `${currentIndent}- ${node.name}: ${formatValue(node.value, depth + 1)}`;
+    case 'added':
+      return `${currentIndent}+ ${node.name}: ${formatValue(node.value, depth + 1)}`;
+    case 'updated':
+      return `${currentIndent}- ${node.name}: ${formatValue(node.oldValue, depth + 1)}\n${currentIndent}+ ${node.name}: ${formatValue(node.newValue, depth + 1)}`;
+    default:
+      throw new Error(`Unknown difference: '${currentStatus}'!`);
+  }
 };
 
 export default stylish;
