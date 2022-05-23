05b0b0229c75512099d9c691d70efdd86a7dce3b
Dmitry Kokorev <superflick3@gmail.com>
2022-05-23 13:36:36 +0300

    Refactoring findDiff.js

--- src/findDiff.js
+++ src/findDiff.js
@@ -7,23 +7,23 @@ const findDiff = (obj1, obj2) => {
   const keys = _.union(firstKeys, secondKeys);
   const sortedKeys = _.sortBy(keys);
 
-  const result = sortedKeys.reduce((acc, key) => {
+  const result = sortedKeys.map((key) => {
     if (_.has(obj1, key) && !_.has(obj2, key)) {
-      return [...acc, { name: key, status: 'removed', value: obj1[key] }];
+      return { name: key, status: 'removed', value: obj1[key] };
     }
     if (!_.has(obj1, key) && _.has(obj2, key)) {
-      return [...acc, { name: key, status: 'added', value: obj2[key] }];
+      return { name: key, status: 'added', value: obj2[key] };
     }
-    if ((typeof obj1[key] === 'object') && (typeof obj2[key] === 'object')) {
-      return [...acc, { name: key, status: 'nested', children: findDiff(obj1[key], obj2[key]) }];
+    if (_.isPlainObject(obj1[key]) && _.isPlainObject(obj2[key])) {
+      return { name: key, status: 'nested', children: findDiff(obj1[key], obj2[key]) };
     }
     if (!_.isEqual(obj1[key], obj2[key])) {
-      return [...acc, {
+      return {
         name: key, status: 'updated', oldValue: obj1[key], newValue: obj2[key],
-      }];
+      };
     }
-    return [...acc, { name: key, status: 'unchanged', value: obj1[key] }];
-  }, []);
+    return { name: key, status: 'unchanged', value: obj1[key] };
+  });
 
   return { status: 'root', children: result };
 };
