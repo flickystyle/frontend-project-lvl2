e6ae9adbd721b4d6b75fd47d75eecf086a3b3c7d
Dmitry Kokorev <superflick3@gmail.com>
2022-05-23 13:37:14 +0300

    Refactoring plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,18 +8,18 @@ const stringify = (value) => {
   return value;
 };
 
-const plain = (node, separator = '', ancestry = '') => {
+const plain = (node, ancestry = []) => {
   const currentStatus = node.status;
   const currentChild = node.children;
-  const newAncestry = `${ancestry}${separator}${node.name}`;
-  const newSeparator = '.';
+  const makeAncestry = [...ancestry, node.name];
+  const newAncestry = makeAncestry.slice(1).join('.');
 
   switch (currentStatus) {
     case 'root':
-      return currentChild.map((child) => plain(child))
-        .filter((line) => line.length !== 0).join('\n');
+      return currentChild.map((child) => plain(child, makeAncestry))
+        .join('\n');
     case 'nested':
-      return currentChild.children.map((child) => plain(child, newSeparator, `${newAncestry}`))
+      return currentChild.children.map((child) => plain(child, makeAncestry))
         .filter((line) => line.length !== 0).join('\n');
     case 'unchanged':
       return '';
